5df220653cd023a24a00ced99954b40f21ed5b2e
Marwa Saleh <extern.msaleh@novomind.com>
2020-10-22 01:22:05 +0200

    adding constructor

--- PhoneBookService/src/com/phonebookservice/server/Main.java
+++ PhoneBookService/src/com/phonebookservice/server/Main.java
@@ -1,14 +1,16 @@
 package com.phonebookservice.server;
 
 public class Main {
+  
+  private Main() {
+  }
 
   /**
    * Checks for value.
-   * 
+   *
    * @return true
    */
   public static boolean checkValue() {
     return true;
   }
-
 }
