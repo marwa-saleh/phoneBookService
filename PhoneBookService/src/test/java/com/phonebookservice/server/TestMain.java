c01fe0fc526f46a88332e45e114d0a6e5fcca36e
Marwa Saleh <extern.msaleh@novomind.com>
2020-10-22 00:41:07 +0200

    fix build errors

---
+++ PhoneBookService/src/test/java/com/phonebookservice/server/TestMain.java
@@ -0,0 +1,18 @@
+package test.java.com.phonebookservice.server;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.phonebookservice.server.Main;
+
+public class TestMain {
+  
+  /**
+   * test method checks for value.
+   */
+  @Test
+  public void testMain() {
+    assertEquals(Boolean.TRUE, Main.checkValue());
+  }
+}
