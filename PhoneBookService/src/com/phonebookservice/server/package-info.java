5049c90522fb0073923565a265b6ef17c43d051d
Marwa Saleh <extern.msaleh@novomind.com>
2020-10-22 01:22:05 +0200

    adding constructor

--- PhoneBookService/src/com/phonebookservice/server/package-info.java
+++ PhoneBookService/src/com/phonebookservice/server/package-info.java
@@ -1,1 +1,11 @@
+/**
+ * server classes used to produce ....
+ * <p>
+ * These classes contain the ......
+ * </p>
+ *
+ * @since 1.0
+ * @author somebody
+ * @version 1.0
+ */
 package com.phonebookservice.server;
