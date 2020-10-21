576adaab86b1c226315405510bc276c010e1986c
Marwa Saleh <extern.msaleh@novomind.com>
2020-10-21 20:34:21 +0200

    add test case

---
+++ PhoneBookService/src/com/phonebookservice/server/Main.java
@@ -0,0 +1,14 @@
+package com.phonebookservice.server;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static boolean checkValue() {
+		return true;
+	}
+
+}
