6010312016c486846c64b0cec4dd68066e72f90c
Marwa Saleh <extern.msaleh@novomind.com>
2020-10-23 00:07:10 +0200

    adding sun_checks.xml

---
+++ PhoneBookService/src/main/java/com/phonebookservice/server/Main.java
@@ -0,0 +1,16 @@
+package main.java.com.phonebookservice.server;
+
+public class Main {
+
+	private Main() {
+	}
+
+	/**
+	 * Checks for value.
+	 *
+	 * @return true
+	 */
+	public static boolean checkValue() {
+		return true;
+	}
+}
