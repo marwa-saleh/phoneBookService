cd7bc372888321f8cb071da8df2f41cabb636c56
Marwa Saleh <extern.msaleh@novomind.com>
2020-10-23 00:34:35 +0200

    remove package-info.java

--- PhoneBookService/src/main/java/com/phonebookservice/server/Main.java
+++ PhoneBookService/src/main/java/com/phonebookservice/server/Main.java
@@ -1,16 +1,16 @@
 package main.java.com.phonebookservice.server;
 
-public class Main {
+public final class Main {
 
-	private Main() {
-	}
+    private Main() {
+    }
 
-	/**
-	 * Checks for value.
-	 *
-	 * @return true
-	 */
-	public static boolean checkValue() {
-		return true;
-	}
+    /**
+     * Checks for value.
+     *
+     * @return true
+     */
+    public static boolean checkValue() {
+        return true;
+    }
 }
