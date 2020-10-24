d06415e512c6faed8e7e78a14df81ac2abcd8320
manoosa <eng.marwa.saleh.92@gmail.com>
2020-10-25 00:37:04 +0200

    Create superlinter.yml (#24)
    * Create superlinter.yml
    
    * add test case
    
    * update yml file
    
    * remove line
    
    * adjust yml file
    
    * fix build errors
    
    * fix Line has trailing spaces.
    
    * adding constructor
    
    * adding sun_checks.xml
    
    * remove package-info.java
    
    * adjust yml file
    
    * adjust to junit 5
    
    * adjust xml file
    
    * adjust xml file to surefire-reports
    
    * adjust xml file
    
    * add surefire in yml
    
    * add surefire in yml
    
    * add build in yml
    
    * add build in yml
    
    * add mvn in yml
    
    * add mvn in yml
    
    * add path of pom in yml
    
    * generate pom xml
    
    * adjust checkout in yml
    
    * adjust project
    
    * remove package.info
    
    * add coverage report
    
    Co-authored-by: Marwa Saleh <extern.msaleh@novomind.com>

---
+++ src/main/java/com/phonebookservice/server/Main.java
@@ -0,0 +1,16 @@
+package main.java.com.phonebookservice.server;
+
+public final class Main {
+
+    private Main() {
+    }
+
+    /**
+     * Checks for value.
+     *
+     * @return true
+     */
+    public static boolean checkValue() {
+        return true;
+    }
+}
