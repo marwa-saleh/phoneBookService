9c1bc4cfc16931b3cd07648b7335ed1c6e52210a
Marwa Saleh <extern.msaleh@novomind.com>
2020-10-24 18:57:30 +0200

    adjust project

--- src/main/java/com/phonebookservice/server/Main.java
+++ src/main/java/com/phonebookservice/server/Main.java
@@ -13,13 +13,4 @@ public final class Main {
     public static boolean checkValue() {
         return true;
     }
-
-    /**
-     * Checks for value.
-     *
-     * @return false
-     */
-    public static boolean checkForValue() {
-        return false;
-    }
 }
